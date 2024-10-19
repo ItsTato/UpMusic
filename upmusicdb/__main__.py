3a34d1fa841b6e3ee97ec91bd73472860128d32d
ItsTato <itstato.official@gmail.com>
2024-10-19 13:16:54 -0600

    Finish the "add" command in DB CLI

--- upmusicdb/__main__.py
+++ upmusicdb/__main__.py
@@ -32,5 +32,30 @@ def main(argv:list[str],argc:int):
 					print("[!] Image ID is invalid")
 					exit()
 				controller.newArtist(name,pfp)
+				exit()
+			if argv[2] == "audio":
+				audioFileName:str = f"./{argv[argc-1]}"
+				if not path.exists(audioFileName):
+					print("[!] Audio not found")
+					exit()
+				split = audioFileName.rsplit(".",1)
+				audioExtension = split[1]
+				audio:Objects.Audio = controller.newAudio(audioExtension.lower())
+				move(audioFileName,f"./data/audios/{audio.id}.{audio.file_extension}")
+				exit()
+			if argv[2] == "song":
+				name:str = input("Name: ")
+				artist_id:int = int(input("Artist ID: "))
+				thumbnail_id:int = int(input("Thumbnail ID: "))
+				thumbnail:Objects.Image|None = controller.getImage(thumbnail_id)
+				artist:Objects.Artist|None = controller.getArtist(artist_id)
+				if thumbnail is None:
+					print("[!] Image ID is invalid")
+					exit()
+				if artist is None:
+					print("[!] Artist ID is invalid")
+					exit()
+				controller.newSong(name,artist,thumbnail)
+				exit()
 
 main(argv,argc)
